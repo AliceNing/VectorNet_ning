3ab882110e266a85db5a24e9a5a616d3b192d39a
AliceNing <979506572@qq.com>
2021-10-09 15:11:58 +0800

    argodataset

---
+++ dataset-argodataset.py
@@ -0,0 +1,288 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from scipy import sparse
+import os
+import copy
+import pandas as pd
+from typing import List, Dict, Any
+from argoverse.data_loading.argoverse_forecasting_loader import ArgoverseForecastingLoader
+from argoverse.map_representation.map_api import ArgoverseMap
+from skimage.transform import rotate
+
+
+class ArgoDataset(Dataset):
+    def __init__(self, split, config, train=True):
+        self.config = config
+        self.train = train
+
+        if 'preprocess' in config and config['preprocess']:#加载预处理好的数据
+            if train:
+                self.split = np.load(self.config['preprocess_train'], allow_pickle=True)
+            else:
+                self.split = np.load(self.config['preprocess_val'], allow_pickle=True)
+        else:#第一次数据预处理
+            self.avl = ArgoverseForecastingLoader(split)
+            self.avl.seq_list = sorted(self.avl.seq_list)
+            self.am = ArgoverseMap()
+            
+    def __getitem__(self, idx):
+
+        '''lanegcn'''
+        #加载处理好的数据
+        if 'preprocess' in self.config and self.config['preprocess']:
+            data = self.split[idx]
+
+            if self.train and self.config['rot_aug']:
+                new_data = dict()
+                for key in ['city', 'norm_center', 'gt_preds', 'has_preds']:
+                    if key in data:
+                        new_data[key] = ref_copy(data[key])
+
+                dt = np.random.rand() * self.config['rot_size']#np.pi * 2.0
+                theta = data['theta'] + dt
+                new_data['theta'] = theta
+                new_data['rot'] = np.asarray([
+                    [np.cos(theta), -np.sin(theta)],
+                    [np.sin(theta), np.cos(theta)]], np.float32)
+
+                rot = np.asarray([
+                    [np.cos(-dt), -np.sin(-dt)],
+                    [np.sin(-dt), np.cos(-dt)]], np.float32)
+                new_data['feats'] = data['feats'].copy()
+                new_data['feats'][:, :, :2] = np.matmul(new_data['feats'][:, :, :2], rot)
+                new_data['ctrs'] = np.matmul(data['ctrs'], rot)
+
+                graph = dict()
+                for key in ['num_nodes', 'turn', 'control', 'intersect', 'pre', 'suc', 'lane_idcs', 'left_pairs', 'right_pairs', 'left', 'right']:
+                    graph[key] = ref_copy(data['graph'][key])
+                graph['ctrs'] = np.matmul(data['graph']['ctrs'], rot)
+                graph['feats'] = np.matmul(data['graph']['feats'], rot)
+                new_data['graph'] = graph
+                data = new_data
+            else:
+                new_data = dict()
+                for key in ['city', 'norm_center', 'gt_preds', 'has_preds', 'theta', 'rot', 'feats', 'ctrs', 'graph']:
+                    if key in data:
+                        new_data[key] = ref_copy(data[key])
+                data = new_data
+
+            return data
+
+        #第一次数据预处理
+        '''data:  
+        X: xs,ys,xe,ye,type,att1,att2,att3,id
+        y: future step, pre-step,  time-step,2
+        ROT:
+        NORM_CENTER:
+        '''
+        data_tmp = self.read_agt_obj_data(idx)
+        data_tmp = self.get_obj_feats(data_tmp)
+        data = self.read_lane_data(data_tmp)
+        return data
+    
+    def __len__(self):
+        if 'preprocess' in self.config and self.config['preprocess']:
+            return len(self.split)
+        else:
+            return len(self.avl)
+
+    def read_agt_obj_data(self, idx):
+        city = copy.deepcopy(self.avl[idx].city)
+        traj_df = copy.deepcopy(self.avl[idx].seq_df)
+        # traj_df: TIMESTAMP,TRACK_ID,OBJECT_TYPE,X,Y,CITY_NAME
+
+        '时间戳归一化'
+        traj_df['TIMESTAMP'] -= np.min(traj_df['TIMESTAMP'].values)
+        traj_timestap = np.sort(np.unique(traj_df['TIMESTAMP'].values))
+        traj_len = traj_timestap.shape[0]
+        steps = [mapping[x] for x in df['TIMESTAMP'].values]
+        steps = np.asarray(steps, np.int64)
+
+        'agent traj'
+        all_trajs = np.concatenate((
+            traj_df.X.to_numpy().reshape(-1, 1),
+            traj_df.Y.to_numpy().reshape(-1, 1)), 1)
+
+        all_objs = traj_df.groupby(['TRACK_ID', 'OBJECT_TYPE']).groups
+        # 统计不同的object和其唯一ID（Keys，0:id,1:type），索引（value）
+        keys = list(all_objs.keys())
+        obj_type = [x[1] for x in keys]
+        agt_idx = obj_type.index('AGENT')
+        idcs = all_objs[keys[agt_idx]]
+
+        # 拿到所有的AGENT
+        agt_traj = all_trajs[idcs]
+        agt_timestamp = traj_timestamp[idcs]
+        agt_step = steps[idcs]
+
+        'agent 的上下文ctx_obj traj'
+        del keys[agt_idx]
+        ctx_trajs, ctx_timestamp = [], []
+        for key in keys:  # keys[('id',type)...]
+            idcs = objs[key]
+            ctx_trajs.append(trajs[idcs])
+            ctx_timestamp.append(traj_timestamp[idcs])
+            ctx_step = steps[idcs]
+
+        '旋转矩阵'
+        norm_center = data['trajs'][0][19].copy().astype(np.float32)
+        # AGENT的第20个时刻位置做为norm_center
+        pre = data['trajs'][0][18] - norm_center
+        theta = np.pi - np.arctan2(pre[1], pre[0])
+        rot = np.asarray([
+            [np.cos(theta), -np.sin(theta)],
+            [np.sin(theta), np.cos(theta)]], np.float32)
+
+        data_tmp = dict()
+        data_tmp['city'] = city
+        # AGENT对象放在第一个，其余是其上下文
+        data_tmp['trajs'] = [agt_traj] + ctx_trajs  # obj_num, step_num, 2
+        data_tmp['timestamp'] = [agt_timestamp] + ctx_timestamp
+        data_tmp['step'] = [agt_step] + ctx_step
+        data_tmp['norm_center'] = norm_center
+        data_tmp['rot'] = rot
+        data_tmp['theta'] = theta
+        return data_tmp
+
+    def get_obj_feats(self, data):
+        poly_feats, gt_preds, has_preds = [], [], []
+        flag = true
+        id = 0
+        for traj, step ,timestamp, norm_center in zip(data['trajs'], data['steps'], data['timestamp'], data['norm_center']):
+            if 19 not in step:  # 删掉step不够20的数据
+                continue
+            # 重新按时间排了下序
+            idcs = step.argsort()
+            step = step[idcs]
+            traj = traj[idcs]
+            timestamp = timestamp[idcs]
+
+            '''处理预测数据   step不够的补0，has_pred = 0表示补0的
+            per-step offsets, 从norm-center开始
+            '''
+            gt_pred = np.zeros((30, 2), np.float32)
+            has_pred = np.zeros(30, np.bool)
+            future_mask = np.logical_and(step >= 20, step < 50)
+
+            post_step = step[future_mask] - 20# future step 从0开始计数
+            post_traj = traj[future_mask] - norm_center
+            gt_pred[post_step] = post_traj
+            has_pred[post_step] = 1
+
+            gt_preds.append(gt_pred)
+            has_preds.append(has_pred)
+
+            '''处理观察到的数据
+            speed   变成vector
+            '''
+            obs_mask = step < 20
+            obs_step = step[obs_mask]
+            obs_traj = traj[obs_mask]
+            obs_timestamp = timestamp[obs_mask]
+
+            # 特征处理成9维的，xs,ys,xe,ye,type,att1,att2,att3,id
+            poly_feat = np.zeros((19, 9), np.float32)
+
+            feat = np.zeros((20, 2), np.float32)
+            feat[obs_step] = np.matmul(rot, (obs_traj - norm_center.reshape(-1, 2)).T).T
+
+            # 删掉超范围的
+            x_min, x_max, y_min, y_max = self.config['pred_range']
+            if feat[-1, 0] < x_min or feat[-1, 0] > x_max or feat[-1, 1] < y_min or feat[-1, 1] > y_max:
+                continue
+
+            type = 1
+            if flag:
+                type = 0 #只有第一次agent 时为0，其他obj为1
+                flag = False
+
+            #speed
+            move = np.hstack((feat[:-1], feat[1:]))
+            speed = [np.sqrt(x ** 2 + y ** 2) for x, y in zip((move[0]-move[2]), (move[1]-move[3]))]
+
+            poly_feat[:,0:4] = np.hstack((feat[:-1], feat[1:]))  #xs,ys,xe,ye
+            poly_feat[:,4] = type  # type: agent 0, obj 1, lane 2
+            poly_feat[:,5] = obs_timestamp[:-1]  # att1 start time
+            poly_feat[:,6] = obs_timestamp[1 :]  # att2 end time
+            poly_feat[:,7] = speed  # att3 speed
+            poly_feat[:,8] = id  # id
+
+            id += 1
+            poly_feats.append(poly_feat)
+
+        # poly_feats = np.asarray(poly_feats, np.float32)
+        gt_preds = np.asarray(gt_preds, np.float32)
+        has_preds = np.asarray(has_preds, np.bool)
+
+        data['poly_feats'] = poly_feats
+        data['gt_preds'] = gt_preds
+        data['has_preds'] = has_preds
+        data['item_num'] = id
+        return data
+
+    def read_lane_data(self, data):
+        id = data['item_num']
+        x_min, x_max, y_min, y_max = self.config['pred_range']
+        radius = max(abs(x_min), abs(x_max)) + max(abs(y_min), abs(y_max))
+        lane_ids = self.am.get_lane_ids_in_xy_bbox(data['norm_center'][0], data['norm_center'][1], data['city'],
+                                                   radius)  # Orig就是AGENT的第20个时刻位置
+        for lane_id in lane_ids:
+            traffic_control = am.lane_has_traffic_control_measure(
+                lane_id, data['city'])
+            is_intersection = am.lane_is_in_intersection(lane_id, data['city'])
+            lane = self.am.city_lane_centerlines_dict[data['city']][lane_id]
+            centerlane = am.get_lane_segment_centerline(lane_id, data['city'])  # 10,3维 包括高度
+            # normalize to last observed timestamp point of agent and rot
+            centerlane[:, :2] = np.matmul(data['rot'], (centerlane[:, :2] - data['norm_center']).T).T
+
+            """得到lane的左右车道线坐标  调用现成的方法"""
+            pts = am.get_lane_segment_polygon(lane_id, data['city'])
+            pts_len = (pts.shape[0] - 1) // 2
+            if pts_len != 10:
+                print(pts_len)
+            lane_1 = np.matmul(rot, (pts[:pts_len, 0:2] - data['norm_center']).T).T
+            lane_2 = np.matmul(rot, (pts[pts_len:2 * pts_len, 0:2] - data['norm_center']).T).T
+
+            # lane1_feature:  xs,ys,xe,ye,type,att1,att2,att3,id
+            poly_feat = np.zeros((9, 9), np.float32)
+            poly_feat[:, 0:4] = np.hstack((lane_1[0:(pts_len - 1)], lane_1[1:]))  # xs,ys,xe,ye
+            poly_feat[:, 4] = 2  # type: agent 0, obj 1, lane 2
+            poly_feat[:, 5] = traffic_control  # att1 traffic_control
+            if lane.turn_direction == 'LEFT':  # att2 direction
+                poly_feat[:, 6] = 1
+            elif lane.turn_direction == 'RIGHT':
+                poly_feat[:, 6] = 2
+            else:
+                poly_feat[:, 6] = 0
+            poly_feat[:, 7] = is_intersection  # att3 intersection
+            poly_feat[:, 8] = id  # id
+
+            data['poly_feats'].append(poly_feat)
+            id +=1
+
+            # lane2_feature:  xs,ys,xe,ye,type,att1,att2,att3,id
+            poly_feat1 = np.zeros((9, 9), np.float32)
+            poly_feat1[:, 0:4] = np.hstack((lane_2[0:(pts_len - 1)], lane_2[1:]))  # xs,ys,xe,ye
+            poly_feat1[:, 4] = 2  # type: agent 0, obj 1, lane 2
+            poly_feat1[:, 5] = traffic_control  # att1 traffic_control
+            if lane.turn_direction == 'LEFT':  # att2 direction
+                poly_feat1[:, 6] = 1
+            elif lane.turn_direction == 'RIGHT':
+                poly_feat1[:, 6] = 2
+            else:
+                poly_feat1[:, 6] = 0
+            poly_feat1[:, 7] = is_intersection  # att3 intersection
+            poly_feat1[:, 8] = id  # id
+
+            data['poly_feats'].append(poly_feat1)
+            id += 1
+
+            data['item_num'] = id
+            data['poly_feats'] = np.asarray(data['poly_feats'], np.float32)
+
+        return data
+
+
+
--- preprocess.py
+++ preprocess.py
@@ -33,6 +33,7 @@ config["DATA_DIR"] = './data'
 config["OBS_LEN"] = 20
 config["Preprocess_DATA_DIR"] = './preprosessed_data'
 config["query_bbox"] = [-100, 100, -100, 100]
+config["viz"] = False
 
 
 def compute_feature_for_one_seq(traj_df, am, config, viz = False, folder_name = "") -> List[List]:
@@ -109,7 +110,6 @@ def compute_feature_for_one_seq(traj_df, am, config, viz = False, folder_name = 
         plt.show()
 
     return [agent_feature, obj_feature_ls, lane_feature_ls, norm_center, rot]
-
 
 def trans_gt_offset_format(gt):
     """#变成每一步移动的距离
@@ -300,3 +300,308 @@ if __name__ == "__main__":
             # print(pd.DataFrame(df['POLYLINE_FEATURES'].values[0]).describe())
         with open(os.path.join(config["Preprocess_DATA_DIR"], f"{folder}-rot_dict.pkl"), 'wb') as f:
             pickle.dump(rot_dict, f, pickle.HIGHEST_PROTOCOL)
+
+
+
+import sys
+import torch
+from torch.utils.data import dataloader
+from torch.multiprocessing import reductions
+from multiprocessing.reduction import ForkingPickler
+
+default_collate_func = dataloader.default_collate
+
+def default_collate_override(batch):
+  dataloader._use_shared_memory = False
+  return default_collate_func(batch)
+
+setattr(dataloader, 'default_collate', default_collate_override)
+
+for t in torch._storage_classes:
+  if sys.version_info[0] == 2:
+    if t in ForkingPickler.dispatch:
+        del ForkingPickler.dispatch[t]
+  else:
+    if t in ForkingPickler._extra_reducers:
+        del ForkingPickler._extra_reducers[t]
+
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, root_path)
+
+
+def main():
+    # Import all settings for experiment.
+    args = parser.parse_args()
+    model = import_module(args.model)
+    print(args.model)
+    config, *_ = model.get_model()
+
+    config["preprocess"] = False  # we use raw data to generate preprocess data
+    config["val_workers"] = 32
+    config["workers"] = 32
+    config['cross_dist'] = 6
+    config['cross_angle'] = 0.5 * np.pi
+
+    os.makedirs(os.path.dirname(config['preprocess_train']), exist_ok=True)
+
+
+
+    # val(config)
+    # test(config)
+    train(config)
+
+
+def train(config):
+    # Data loader for training set
+    dataset = Dataset(config["train_split"], config, train=True)
+    train_loader = DataLoader(
+        dataset,
+        batch_size=config["batch_size"],
+        num_workers=config["workers"],  #加载数据进程数
+        shuffle=False,
+        collate_fn=collate_fn,
+        pin_memory=True, #拷贝数据到cuda
+        drop_last=False,
+    )
+
+    stores = [None for x in range(2016)]#205942   little dataset:  63*32
+    t = time.time()
+    for i, data in enumerate(tqdm(train_loader)): #batch_num 0-6435
+        # little dataset
+        if i > 62:
+            break
+
+        data = dict(data)
+        for j in range(len(data["idx"])):#batch 0-31
+            store = dict()
+            for key in [
+                "idx",
+                "city",
+                "feats",
+                "ctrs",
+                "orig",
+                "theta",
+                "rot",
+                "gt_preds",
+                "has_preds",
+                "graph",
+            ]:
+                store[key] = to_numpy(data[key][j])
+                if key in ["graph"]:
+                    store[key] = to_int16(store[key])
+
+            stores[store["idx"]] = store
+
+        if (i + 1) % 100 == 0:
+            print(i, time.time() - t)
+            t = time.time()
+
+
+    dataset = PreprocessDataset(stores, config, train=True)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=config['batch_size'],
+        num_workers=config['workers'],
+        shuffle=False,
+        collate_fn=from_numpy,
+        pin_memory=True,
+        drop_last=False)
+
+    modify(config, data_loader, config["preprocess_train"])
+
+
+def val(config):
+    # Data loader for validation set
+    dataset = Dataset(config["val_split"], config, train=False)
+    val_loader = DataLoader(
+        dataset,
+        batch_size=config["val_batch_size"],
+        num_workers=config["val_workers"],
+        shuffle=False,
+        collate_fn=collate_fn,
+        pin_memory=True,
+    )
+    stores = [None for x in range(2016)]  #39472
+
+    t = time.time()
+    for i, data in enumerate(tqdm(val_loader)):
+        # little dataset
+        if i > 62:
+            break
+        data = dict(data)
+        for j in range(len(data["idx"])):
+            store = dict()
+            for key in [
+                "idx",
+                "city",
+                "feats",
+                "ctrs",
+                "orig",
+                "theta",
+                "rot",
+                "gt_preds",
+                "has_preds",
+                "graph",
+            ]:
+                store[key] = to_numpy(data[key][j])
+                if key in ["graph"]:
+                    store[key] = to_int16(store[key])
+            stores[store["idx"]] = store
+
+        if (i + 1) % 100 == 0:
+            print(i, time.time() - t)
+            t = time.time()
+
+    dataset = PreprocessDataset(stores, config, train=False)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=config['batch_size'],
+        num_workers=config['workers'],
+        shuffle=False,
+        collate_fn=from_numpy,
+        pin_memory=True,
+        drop_last=False)
+
+    modify(config, data_loader,config["preprocess_val"])
+
+
+def test(config):
+    dataset = Dataset(config["test_split"], config, train=False)
+    test_loader = DataLoader(
+        dataset,
+        batch_size=config["val_batch_size"],
+        num_workers=config["val_workers"],
+        shuffle=False,
+        collate_fn=collate_fn,
+        pin_memory=True,
+    )
+    stores = [None for x in range(1024)] #78143
+
+    t = time.time()
+    for i, data in enumerate(tqdm(test_loader)):
+        # little dataset
+        if i > 31:
+            break
+        data = dict(data)
+        for j in range(len(data["idx"])):
+            store = dict()
+            for key in [
+                "idx",
+                "city",
+                "feats",
+                "ctrs",
+                "orig",
+                "theta",
+                "rot",
+                "graph",
+            ]:
+                store[key] = to_numpy(data[key][j])
+                if key in ["graph"]:
+                    store[key] = to_int16(store[key])
+            stores[store["idx"]] = store
+
+        if (i + 1) % 100 == 0:
+            print(i, time.time() - t)
+            t = time.time()
+
+    dataset = PreprocessDataset(stores, config, train=False)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=config['batch_size'],
+        num_workers=config['workers'],
+        shuffle=False,
+        collate_fn=from_numpy,
+        pin_memory=True,
+        drop_last=False)
+
+    modify(config, data_loader,config["preprocess_test"])
+
+
+def to_numpy(data):
+    """Recursively transform torch.Tensor to numpy.ndarray.
+    """
+    if isinstance(data, dict):
+        for key in data.keys():
+            data[key] = to_numpy(data[key])
+    if isinstance(data, list) or isinstance(data, tuple):
+        data = [to_numpy(x) for x in data]
+    if torch.is_tensor(data):
+        data = data.numpy()
+    return data
+
+
+def to_int16(data):
+    if isinstance(data, dict):
+        for key in data.keys():
+            data[key] = to_int16(data[key])
+    if isinstance(data, list) or isinstance(data, tuple):
+        data = [to_int16(x) for x in data]
+    if isinstance(data, np.ndarray) and data.dtype == np.int64:
+        data = data.astype(np.int16)
+    return data
+
+
+def modify(config, data_loader, save):
+    t = time.time()
+    store = data_loader.dataset.split
+    for i, data in enumerate(data_loader):
+        data = [dict(x) for x in data]
+
+        out = []
+        for j in range(len(data)):
+            # preprocess(data[j], config['cross_dist'])
+            out.append(preprocess(to_long(gpu(data[j])), config['cross_dist']))  #'cross_dist': 6
+
+        for j, graph in enumerate(out):
+            idx = graph['idx']
+            store[idx]['graph']['left'] = graph['left']
+            store[idx]['graph']['right'] = graph['right']
+
+        if (i + 1) % 100 == 0:
+            print((i + 1) * config['batch_size'], time.time() - t)
+            t = time.time()
+
+    # write preprocessed  data  !!!
+    f = open(os.path.join(root_path, 'preprocess', save), 'wb')
+    pickle.dump(store, f, protocol=pickle.HIGHEST_PROTOCOL)
+    f.close()
+
+class PreprocessDataset():
+    def __init__(self, split, config, train=True):
+        self.split = split
+        self.config = config
+        self.train = train
+
+    def __getitem__(self, idx):
+        from data import from_numpy, ref_copy
+        data = self.split[idx]
+        graph = dict()
+        for key in ['lane_idcs', 'ctrs', 'pre_pairs', 'suc_pairs', 'left_pairs', 'right_pairs', 'feats']:
+            # graph[key] = ref_copy(data['graph'][key])
+            # Nonetype data
+            try:
+                graph[key] = ref_copy(data['graph'][key])
+            except:
+                continue
+        graph['idx'] = idx
+        return graph
+
+    def __len__(self):
+        return len(self.split)
+
+def to_long(data):
+    if isinstance(data, dict):
+        for key in data.keys():
+            data[key] = to_long(data[key])
+    if isinstance(data, list) or isinstance(data, tuple):
+        data = [to_long(x) for x in data]
+    if torch.is_tensor(data) and data.dtype == torch.int16:
+        data = data.long()
+    return data
+
+def worker_init_fn(pid):
+    np_seed = hvd.rank() * 1024 + int(pid)
+    np.random.seed(np_seed)
+    random_seed = np.random.randint(2 ** 32 - 1)
+    random.seed(random_seed)
